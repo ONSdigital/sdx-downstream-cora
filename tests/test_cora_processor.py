3fda3ae645a7ea4b7ec281cdf185f11f731bcc98
Eleanor Deal <eleanor.deal@gmail.com>
2017-01-11 15:44:15 +0000

    Initial app setup

---
+++ tests/test_cora_processor.py
@@ -0,0 +1,36 @@
+import unittest
+from unittest.mock import MagicMock
+import json
+import logging
+from structlog import wrap_logger
+from app.processors.cora_processor import CoraProcessor
+from tests.test_data import cora_survey
+
+logger = wrap_logger(logging.getLogger(__name__))
+
+
+class TestCoraProcessor(unittest.TestCase):
+
+    def setUp(self):
+        survey = json.loads(cora_survey)
+        self.processor = CoraProcessor(logger, survey)
+
+    def test_tx_id_should_be_set(self):
+        self.assertIsNotNone(self.processor.tx_id)
+
+    def test_transform_failure(self):
+        self.processor.transform = MagicMock(return_value=None)
+        result = self.processor.process()
+        self.assertFalse(result)
+
+    def test_process_failure(self):
+        self.processor.transform = MagicMock(return_value="success")
+        self.processor.deliver_zip = MagicMock(return_value=False)
+        result = self.processor.process()
+        self.assertFalse(result)
+
+    def test_process_success(self):
+        self.processor.transform = MagicMock(return_value="success")
+        self.processor.deliver_zip = MagicMock(return_value=True)
+        result = self.processor.process()
+        self.assertTrue(result)
