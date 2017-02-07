14b8124675e9d4bd4f31b0f8705e3f7af29f9aa6
Eleanor Deal <eleanor.deal@gmail.com>
2017-02-07 16:27:20 +0000

    Rename env var for queue name to CORA_NOTIFICATIONS_QUEUE

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ FTP_PASS = os.getenv('FTP_PASS')
 FTP_FOLDER = os.getenv('FTP_FOLDER', '/')
 FTP_HEARTBEAT_FOLDER = os.getenv('FTP_HEARTBEAT_FOLDER', '/heartbeat')
 
-RABBIT_QUEUE = os.getenv('RABBIT_QUEUE', 'sdx-cora-survey-notifications')
+RABBIT_QUEUE = os.getenv('CORA_NOTIFICATIONS_QUEUE', 'sdx-cora-survey-notifications')
 RABBIT_EXCHANGE = os.getenv('RABBITMQ_EXCHANGE', 'message')
 
 RABBIT_URL = 'amqp://{user}:{password}@{hostname}:{port}/{vhost}'.format(
