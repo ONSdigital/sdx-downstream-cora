d477d539ae93ea72d44e2eacfffca9116e65ebe1
Tejas Patel <tejas.patel@ons.gov.uk>
2017-08-15 10:43:31 +0100

    correct logging config

--- app/settings.py
+++ app/settings.py
@@ -5,7 +5,7 @@ from requests.packages.urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
 
 LOGGING_LEVEL = logging.getLevelName(os.getenv('LOGGING_LEVEL', 'DEBUG'))
-LOGGING_FORMAT = "%(asctime)s |%(levelname)s: sdx-downstream-cora: %(message)s"
+LOGGING_FORMAT = "%(asctime)s.%(msecs)06dZ|%(levelname)s: sdx-downstream-cora: %(message)s"
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 APP_TMP = os.path.join(APP_ROOT, 'tmp')
